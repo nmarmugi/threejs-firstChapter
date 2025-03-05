edd789a07d2443ffabe5b31f02669c02fb84b817
Nicola Marmugi <marmu1@hotmail.it>
2025-03-05 20:46:40 +0100

    deploy

--- src/script.js
+++ src/script.js
@@ -2,13 +2,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
-import GUI from 'lil-gui'
 
 /**
  * Base
  */
-// Debug
-const gui = new GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
